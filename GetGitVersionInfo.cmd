bd753cee98567470d1b39d5e23859da85217bb4b
Florian Simon <github@verm-luh.de>
2016-07-28 15:28:55 +0200

    added comments

--- GetGitVersionInfo.cmd
+++ GetGitVersionInfo.cmd
@@ -1,14 +1,20 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 set BRANCH=
+REM extract branch name
 for /f %%i in ('git symbolic-ref HEAD 2^> nul') do set BRANCH=%%i
+REM and store in BRANCH variable
 set BRANCH=!BRANCH:~11!
 
+REM read latest abbreviated commit hash
 git log --pretty=format:\"%%h\" -1 > hash
+REM and store in hash variable
 set /p HASH=<hash
 del hash
+REM remove double quotes
 set HASH=!HASH:~1,-1!
 
+REM concat branch and commit hash strings
 echo %BRANCH%-%HASH%
 endlocal
 echo on
