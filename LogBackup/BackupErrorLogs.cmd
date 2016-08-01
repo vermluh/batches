5da19c95fbb5723e6c0837bd5ae254fc7eced0bd
Florian Simon <github@verm-luh.de>
2016-08-01 16:29:41 +0200

    timestamping and comments
    -added timestamp to log file name
    -added comments

--- LogBackup/BackupErrorLogs.cmd
+++ LogBackup/BackupErrorLogs.cmd
@@ -1,12 +1,32 @@
 @echo off
+
 if [%1]==[] goto USAGE
+
+setlocal
+
+REM create target path if it does not exist
 set targetpath=%1
 if not exist %targetpath%\logs md %targetpath%\logs
-for /f "tokens=1,2" %%a in (AddressesAndNames.txt) do (
-  robocopy "\\%%a\error_log" %targetpath%\%%a\error_log /XF "\\%%a\error_log\DONT.DELETE_OR_MOVE" /E /log+:%targetpath%\logs\backup.%%a_%%b.log
+
+REM determine current datetime string
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined currentdate set currentdate=%%x
+set currentdatestring=%currentdate:~0,4%-%currentdate:~4,2%-%currentdate:~6,2%T%currentdate:~8,2%_%currentdate:~10,2%_%currentdate:~12,2%
+
+for /f "tokens=1,2" %%a in (AnlagenMitNamen.txt) do (
+  REM copy files
+  robocopy "\\%%a\error_log" %targetpath%\%%a\error_log /XF "\\%%a\error_log\DONT.DELETE_OR_MOVE" /E /log:%targetpath%\logs\backup_%%b_%currentdatestring%.log
+  
+  REM link machine name to IP address
   if not exist %targetpath%\%%b mklink /D %targetpath%\%%b %targetpath%\%%a"
 )
+
+REM reset current datetime and datestring
+set currentdate=
+set currentdatestring=
+
+endlocal
 goto :eof
+
 :USAGE
-@echo Usage: %0 ^<TARGETPATH^>
+echo Usage: %0 ^<TARGETPATH^>
 exit /B 1
